b594e7ddb61d52ca89149284c93f5e8104e6a1d8
Olli Riikonen <ollie.riikonen@gmail.com>
2020-07-30 09:16:33 +0300

    Center content for sections

--- src/sections/section.js
+++ src/sections/section.js
@@ -9,29 +9,34 @@ const Section = (props) => {
     const { data } = usePalette(props.img)
 
     return (
-        <Parallax  bgImage={props.img} strength={800} className={sectionStyles.parallax}>
+        <Parallax bgImage={props.img} strength={600} className={sectionStyles.parallax}>
         <div id={props.id} className={props.solid} style={{ backgroundColor: data.darkVibrant }}>
 
-            <div className={sectionStyles.sectionBorder}></div>
+                <div className={sectionStyles.sectionBorder}></div>
 
-            <div className={sectionStyles.section}>
-                <div className={sectionStyles.firstColumn}></div>
-                <div className={sectionStyles.transparentColumn}></div>
-                <div className={sectionStyles.midColumn}></div>
-                <div className={sectionStyles.transparentColumn}></div>
-                
-                <div className={sectionStyles.contentColumn}>
-                    <div className={sectionStyles.content} style={{ backgroundColor: data.vibrant }}>
-                        <h1>{props.title}</h1>
-                        <br/>
-                        <div>
-                            {props.children}
+                <div className={sectionStyles.section}>
+                    <div className={sectionStyles.firstColumn}></div>
+                    <div className={sectionStyles.transparentColumn}></div>
+                    <div className={sectionStyles.midColumn}></div>
+                    <div className={sectionStyles.transparentColumn}></div>
+                    
+                    <div className={sectionStyles.contentColumn}></div>
+
+                    <div className={sectionStyles.container}>
+                        <div className={sectionStyles.content} style={{ backgroundColor: data.vibrant }}>
+                            <h1>{props.title}</h1>
+                            <br/>
+                            <div>
+                                {props.children}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className={sectionStyles.sectionBorder}></div>
+                </div>
+
+
+                <div className={sectionStyles.sectionBorder}></div>
+
         </div>
         {/* <Background className="custom-bg"> 
              {props.img}
