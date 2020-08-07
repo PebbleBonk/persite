480de3c19f54c8ce39037db7c89f377ec97f4c41
Olli Riikonen <ollie.riikonen@gmail.com>
2020-08-07 14:53:42 +0300

    Move sections inside the section

--- src/sections/section.js
+++ src/sections/section.js
@@ -16,30 +16,36 @@ const Section = (props) => {
         <Parallax strength={600} className={sectionStyles.parallax}>
         <div id={props.id} className={props.solid} style={sectionColourStyle}>
 
-                <div className={sectionStyles.sectionBorder}></div>
 
                 <div className={sectionStyles.section}>
-                    <div className={sectionStyles.firstColumn}></div>
-                    <div className={sectionStyles.transparentColumn}></div>
-                    <div className={sectionStyles.midColumn}></div>
-                    <div className={sectionStyles.transparentColumn}></div>
-                    
-                    <div className={sectionStyles.contentColumn}></div>
+                    <div className={sectionStyles.sectionBorder}></div>
 
-                    <div className={sectionStyles.container}>
-                        <div className={sectionStyles.content} style={{ backgroundColor: muted }}>
-                            <h1>{props.title}</h1>
-                            <br/>
-                            <div>
-                                {props.children}
+
+                    <div className={sectionStyles.sectionContent}>
+
+                        <div className={sectionStyles.firstColumn}></div>
+                        <div className={sectionStyles.transparentColumn}></div>
+                        <div className={sectionStyles.midColumn}></div>
+                        <div className={sectionStyles.transparentColumn}></div>
+                        
+                        <div className={sectionStyles.contentColumn}></div>
+
+                        <div className={sectionStyles.container}> 
+                            <div className={sectionStyles.content} style={{ backgroundColor: muted }}>
+                                <h1>{props.title}</h1>
+                                <br/>
+                                <div>
+                                    {props.children}
+                                </div>
                             </div>
                         </div>
+
                     </div>
 
+
+                    <div className={sectionStyles.sectionBorder}></div>
                 </div>
 
-
-                <div className={sectionStyles.sectionBorder}></div>
 
         </div>
         <Background style={{width: '100%', height: '100%'}}>
