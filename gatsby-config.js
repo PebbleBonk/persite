5376b635dcbaaaf1703a680de9ef4c72cea11302
Olli Riikonen <ollie.riikonen@gmail.com>
2020-08-10 11:11:43 +0300

    Add scroll animations plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,23 @@ module.exports = {
     `gatsby-plugin-fontawesome-css`,
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-scroll-reveal`,
+      options: {
+          threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
+          once: true, // Defines if animation needs to be launched once
+          disable: false, // Flag for disabling animations
+          
+          // Advanced Options
+          selector: '[data-sal]', // Selector of the elements to be animated
+          animateClassName: 'sal-animate', // Class name which triggers animation
+          disabledClassName: 'sal-disabled', // Class name which defines the disabled state
+          rootMargin: '0% 50%', // Corresponds to root's bounding box margin
+          enterEventName: 'sal:in', // Enter event name
+          exitEventName: 'sal:out', // Exit event name
+      }
+    },
     'gatsby-image',
     {
       resolve: 'gatsby-source-filesystem',
@@ -31,7 +48,6 @@ module.exports = {
         path: path.join(__dirname, 'resources', 'img')
       }
     },
-    'gatsby-plugin-sharp',
     `gatsby-transformer-sharp`,
     {
       resolve: 'gatsby-transformer-remark',
