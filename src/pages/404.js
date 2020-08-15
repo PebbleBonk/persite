7c6c13ed6aabd50937484ff4014bd530b791bef7
Olli Riikonen <ollie.riikonen@gmail.com>
2020-08-15 16:58:44 +0300

    Spice up the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,34 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-
+import layoutStyles from '../styles/layout.module.scss'
+import animationStyles from '../styles/animation.module.scss'
 
 const NotFound = () => {
+
+  const fofStyle = {
+      wrapper: {
+        height: '200px',
+        color: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
+  }
     return (
         <Layout>
-        <h1>It's not me you're looking for. Probably...</h1>
-        <p><Link to='/'>Go home</Link></p>
+            <div className={layoutStyles.fullScreen}>
+                <h1>404</h1>
+                <h3>It's not me you're looking for. Probably...</h3>
+                {/* <br></br>
+                <h1>||</h1> */}
+                <Link to='/' style={fofStyle.wrapper}>
+                    <h1 className={animationStyles.colorFading}>||</h1>
+                    {/* <br/> */}
+                    Go home
+                </Link>
+            </div>
       </Layout>
     )
 }
