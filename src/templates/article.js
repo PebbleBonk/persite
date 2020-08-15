3bd2068879cdda6a177b3bb307348eba5b3d5a55
Olli Riikonen <ollie.riikonen@gmail.com>
2020-08-15 10:22:21 +0300

    Style the articles

--- src/templates/article.js
+++ src/templates/article.js
@@ -4,6 +4,9 @@ import Layout from '../components/layout'
 import { Link, graphql } from 'gatsby'
 import Img from "gatsby-image"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+
 import articleStyles from './article.module.scss'
 
 export const query = graphql`
@@ -34,33 +37,41 @@ export const query = graphql`
 const Article = ({ data, pageContext }) => {
     const { next, prev } = pageContext.navContext
 
+    const navWrap = (
+        <div className={articleStyles.navWrapper}>
+        <div className={articleStyles.navPrev}>
+        {prev && (
+            <Link to={prev.path}>
+                <FontAwesomeIcon icon={faLongArrowAltLeft} /> {prev.title}
+            </Link>
+        )}
+        </div>
+        {/* <div className={articleStyles.navBack}>
+            <Link to="/">Home</Link>
+        </div> */}
+        <div className={articleStyles.navNext}>
+        {next && (
+            <Link to={next.path}>
+                {next.title} <FontAwesomeIcon icon={faLongArrowAltRight} /> 
+            </Link>
+        )}
+        </div>
+    </div>
+    )
+
     return (
         <Layout>
-            <div className={articleStyles.navWrapper}>
-                <div className={articleStyles.navPrev}>
-                {prev && (
-                    <Link to={prev.path}>
-                    Previous: {prev.title}
-                    </Link>
-                )}
-                </div>
-                <div className={articleStyles.navBack}>
-                    <Link to="/">Home</Link>
-                </div>
-                <div className={articleStyles.navNext}>
-                {next && (
-                    <Link to={next.path}>
-                    Next: {next.title}
-                    </Link>
-                )}
-                </div>
-            </div>
+            <div className={articleStyles.articleWrapper}>
+                {navWrap}
 
-            <div className={articleStyles.article}>
-                <h1>{data.markdownRemark.frontmatter.title}</h1>
-                <p>{data.markdownRemark.frontmatter.date}</p>
-                <Img fluid={data.markdownRemark.frontmatter.cover.childImageSharp.fluid} alt={data.markdownRemark.frontmatter.title}/>
-                <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
+                <div className={articleStyles.article}>
+                    <Img className={articleStyles.coverImg} fluid={data.markdownRemark.frontmatter.cover.childImageSharp.fluid} alt={data.markdownRemark.frontmatter.title}/>
+                    <h1>{data.markdownRemark.frontmatter.title}</h1>
+                    <p className={articleStyles.date}>{data.markdownRemark.frontmatter.date}</p>
+                    <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
+                </div>
+
+                {navWrap}
             </div>
         </Layout>
         )
