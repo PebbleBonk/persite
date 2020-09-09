5d20be42e605bb788147dbbbd657ce107c486cb6
Olli Riikonen <ollie.riikonen@gmail.com>
2020-09-09 14:00:54 +0300

    Add a photo to the about section

--- src/content/about.js
+++ src/content/about.js
@@ -1,47 +1,69 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from "gatsby-image"
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb, faProjectDiagram, faChartBar, faEye }  from '@fortawesome/free-solid-svg-icons'
 import layoutStyles from '../styles/layout.module.scss'
 
 
+
 const ContentOfAbout = () => {
+    const query = useStaticQuery(graphql`
+        query {
+            fileName: file(relativePath: { eq: "profile/itsame.jpg" }) {
+                childImageSharp {
+                    fluid(maxWidth: 640, maxHeight: 640) {
+                    ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `)
     return (
         <React.Fragment>
-            <div   
-                style={{
-                    maxWidth: "450px",
-                    paddingLeft:"3rem",
-                    // paddingRight:"6rem"
-                }}                     
-                data-sal="slide-up"
-                data-sal-delay="300"
-                data-sal-easing="ease">
-                <p>A broad-skilled software engineer!</p>
-                <p>
-                    Originating from eastern Finland, and currently based in Espoo.
-                </p>
-                <p>
-                    I like to work with things that are new: creating innovative solutions and trying to find
-                    those real-life cheat codes are tasks that bring the best out of me.
-                </p>
-                <p>
-                    Expecially the following inspire me:
-                </p>
+            <div className={layoutStyles.aboutContent}>
+            <div className={layoutStyles.circularPhoto}>
+                <Img fluid={query.fileName.childImageSharp.fluid} alt="" />
             </div>
-            <ul className="fa-ul">
-                <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
-                    <FontAwesomeIcon fixedWidth icon={faLightbulb}/>  Innovation
-                </li>
-                <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
-                    <FontAwesomeIcon fixedWidth icon={faProjectDiagram}/>  Holism
-                </li>
-                <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
-                    <FontAwesomeIcon fixedWidth icon={faEye}/>  Visualisation
-                </li>
-                <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
-                    <FontAwesomeIcon fixedWidth icon={faChartBar}/>  Data
-                </li>
-            </ul>
+            <div>
+                <div   
+                    style={{
+                        maxWidth: "450px",
+                        paddingLeft:"3rem",
+                        // paddingRight:"6rem"
+                    }}                     
+                    // data-sal="slide-up"
+                    // data-sal-delay="300"
+                    data-sal-easing="ease">
+                    <p>A broad-skilled software engineer!</p>
+                    <p>
+                        Originating from eastern Finland, and currently based in Espoo.
+                    </p>
+                    <p>
+                        I like to work with things that are new: creating innovative solutions and trying to find
+                        those real-life cheat codes are tasks that bring out the best in me.
+                    </p>
+                    <p>
+                        Expecially the following inspire me:
+                    </p>
+                </div>
+                <ul className="fa-ul">
+                    <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
+                        <FontAwesomeIcon fixedWidth icon={faLightbulb}/>  Innovation
+                    </li>
+                    <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
+                        <FontAwesomeIcon fixedWidth icon={faProjectDiagram}/>  Holism
+                    </li>
+                    <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
+                        <FontAwesomeIcon fixedWidth icon={faEye}/>  Visualisation
+                    </li>
+                    <li  data-sal="slide-righ" className={layoutStyles.pillButton}>
+                        <FontAwesomeIcon fixedWidth icon={faChartBar}/>  Data
+                    </li>
+                </ul>     
+            </div>
+            </div>
         </React.Fragment>
     )
 }
