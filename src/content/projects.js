e9a8385d9142a4be9fd22d17a8bd69fb4f8d69e0
Olli Riikonen <ollie.riikonen@gmail.com>
2020-09-18 12:54:01 +0300

    Fix a couple of typos

--- src/content/projects.js
+++ src/content/projects.js
@@ -16,7 +16,7 @@ const ContentOfProjects = () => {
                     <p>These are some projects I have spent some significant amount of time on.</p>
                     <p>They have usually risen from some seemingly urgent necessity I have had, 
                         and nicely blown out of proportion as I have grown more keen on expanding 
-                        the scope while having too muh fun implementing them
+                        the scope while having too much fun implementing them
                     </p>
                 </div>
             </FeaturedSection>
@@ -28,7 +28,7 @@ const ContentOfProjects = () => {
                     <h2>Misc. projects</h2>
                     <p>
                     These are some other projects I've worked on, which might not be so extensive, 
-                    but still worth checking out
+                    but still worth checking out!
                     </p>
                 </div>
             </ProjectsSection>
