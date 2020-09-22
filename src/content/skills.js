609a67ee5950891dbb35754f614ad61219108a5b
Olli Riikonen <ollie.riikonen@gmail.com>
2020-09-22 12:24:49 +0300

    Update skills layout

--- src/content/skills.js
+++ src/content/skills.js
@@ -1,38 +1,41 @@
 import React from 'react'
 
 
+const SkillList = (props) => {
+    return (
+        <div style={{padding:"1rem", width: "230px", boxSizing: "border-box"}}
+            data-sal="slide-up" data-sal-delay="300" data-sal-easing="ease">
+            {props.children}
+        </div>
+    )
+}
+
 const ContentOfSkills = () => {
     return (
-        <div>
-            <div 
-                        data-sal="slide-up"
-                        data-sal-delay="300"
-                        data-sal-easing="ease">
+        <div style={{maxWidth: "527px"}}>
+            <div data-sal="slide-up" data-sal-delay="300" data-sal-easing="ease">
                 <p>
-                I have a Master's degree from <b>Aalto University</b> (FI), where I majored in <b>Control, Robotics and Autonomous systems.</b> <br/>
+                    I have a Master's degree from <b>Aalto University</b> (FI), where I majored in <b>Control, Robotics and Autonomous systems.</b> <br/>
                     Additionally, I have work experience from multiple different industries and 
                     excel in learning new things - <b>fast</b>. 
                 </p>
             </div>
 
             <div style={{display: "flex", flexWrap: "wrap"}}>
-                <div style={{padding:"1rem"}}>
-                    <b>Languages I am comfortable with:</b>
-                    <ul>
-                        <li>Python</li>
-                        <li>C/C++</li>
-                        <li>C#</li>
-                        <li>R</li>
-                        <li>Matlab</li>
-                        <li>JavaScript</li>
-                    </ul>
-                </div>
+                <SkillList>
+                        <b>Languages I am fluent in:</b>
+                        <ul>
+                            <li>Python</li>
+                            <li>C/C++</li>
+                            <li>C#</li>
+                            <li>R</li>
+                            <li>Matlab</li>
+                            <li>JavaScript</li>
+                        </ul>
+                </SkillList>
 
-                <div style={{padding:"1rem"}}
-                            data-sal="slide-up"
-                            data-sal-delay="300"
-                            data-sal-easing="ease">
-                    <b >Frameworks I've been tinkering with:</b>
+                <SkillList>
+                    <b >Frameworks I've tinkered with:</b>
                     <ul>
                         <li>React</li>
                         <li>Gatsby</li>
@@ -41,22 +44,27 @@ const ContentOfSkills = () => {
                         <li>Flask</li>
                         <li>Numpy/Pandas</li>
                     </ul>
-                </div>
+                </SkillList>
 
-                <div style={{padding:"1rem"}}
-                            data-sal="slide-up"
-                            data-sal-delay="300"
-                            data-sal-easing="ease">
+                <SkillList>
                     <b>Tools I have discovered:</b>
                     <ul>
                         <li>Git</li>
                         <li>Bash</li>
                         <li>MongoDB</li>
-                        <li>SQL</li>
                         <li>ROS</li>
-                        <li>Raspberry Pi</li>
                     </ul>
-                </div>
+                </SkillList>
+
+                <SkillList>
+                    <b>Hardware I have played with:</b>
+                    <ul>
+                        <li>Raspberry Pi</li>
+                        <li>Diddyborg</li>
+                        <li>Arduino</li>
+                        <li>FLIR cameras</li>
+                    </ul>
+                </SkillList>
             </div>
         </div>
     )
